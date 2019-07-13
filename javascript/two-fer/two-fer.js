90cd7a6860f13eadc5d277845d30ba149b9685be
Viktor Zetterström <viktor.zetterstrom@appliedtechnology.se>
2019-07-14 00:16:58 +0200

    refactor

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,1 +1,1 @@
-export const twoFer = name => (name !== '' ? `One for ${name}, one for me.` : 'One for you, one for me.');
+export const twoFer = (name = 'you') => `One for ${name || 'you'}, one for me.`;
