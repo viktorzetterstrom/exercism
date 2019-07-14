1f06c9ac507082382b6d96bb494903e44357b2bf
Viktor Zetterström <viktor.zetterstrom@appliedtechnology.se>
2019-07-14 10:21:03 +0200

    update solution

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,10 +1,4 @@
-const COLORS = 
+export const COLORS =
   ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-function colorCode(wantedColor) {
-  return COLORS.findIndex(color => color === wantedColor);
-}
-
-
-
-module.exports = { colorCode, COLORS };
+export const colorCode = (wantedColor) => COLORS.indexOf(wantedColor);
