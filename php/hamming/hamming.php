1c70ee1ae9ab303e0df3e470d0361e3c5eb6b685
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-09-17 16:16:27 +0200

    refactored old solutions and solved rna_transcription

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,20 +1,11 @@
 <?php
 
-//
-// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-function distance($a, $b)
-{
-  // Split strings.
+function distance($a, $b) {
   $a_arr = str_split($a);
   $b_arr = str_split($b);
 
-  // Make sure they are of the same length.
   if (count($a_arr) != count($b_arr)) throw new InvalidArgumentException("DNA strands must be of equal length.");
 
-  // Calculate Hamming distance.
   $hammingDistance = 0;
   for ($i=0; $i < count($a_arr); $i++) { 
     if ($a[$i] != $b[$i]) $hammingDistance++;
--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -1,1 +1,10 @@
 <?php
+function toRna($dnaString) {
+  $dnaToRnaTable = [
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U',
+  ];
+  return strtr($dnaString, $dnaToRnaTable);
+}
