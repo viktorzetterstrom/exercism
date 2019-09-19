b58c0a8e3cd645a7598a5cf0b07ba53cbfbc886c
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-09-19 14:35:07 +0200

    start work on roman-numerals php

--- php/roman-numerals/roman-numerals.php
+++ php/roman-numerals/roman-numerals.php
@@ -1,10 +1,17 @@
 <?php
 
 function toRoman(int $number): string {
-  $romanString = '';
-  
-  while($number % 10 != $number) {
-    $numerals = { 'M', 'CM', }
-  }
-  return '';
+  $romanNumeralMap = [
+    0 => '', 1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX',
+    10 => 'X', 20 => 'XX', 30 => 'XXX', 40 => 'XL', 50 => 'L', 60 => 'LX', 70 => 'LXX', 80 => 'LXXX', 90 => 'XC',
+    100 => 'C', 200 => 'CC', 300 => 'CCC', 400 => 'CD', 500 => 'D', 600 => 'DC', 700 => 'DCC', 800 => 'DCCC',
+    900 => 'CM', 1000 => 'M', 2000 => 'MM', 3000 => 'MMM'
+  ];
+
+  $digits = str_explode($number);
+  $roman = array_reduce($digits, function($carry, $item) {
+    $carry .= $romanNumeralMap[$item];
+  }, '');
+
+  return $roman;
 }
