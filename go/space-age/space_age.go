83f7baae90d4e4136393ef16e61dcdb55a6c9661
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-11-17 21:16:45 +0100

    go update space-age

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -3,28 +3,21 @@
 */
 package space
 
-import "strings"
-
 type Planet string
+
+var orbitalPeridods = map[Planet]float64{
+	"Earth":   1.0,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
 
 // Takes seconds and a planet name and returns the years passed on that planet.
 func Age(seconds float64, planet Planet) float64 {
 	earthYears := seconds / (60 * 60 * 24 * 365.25)
-	switch strings.ToLower(string(planet)) {
-	case "mercury":
-		return earthYears / 0.2408467
-	case "venus":
-		return earthYears / 0.61519726
-	case "mars":
-		return earthYears / 1.8808158
-	case "jupiter":
-		return earthYears / 11.862615
-	case "saturn":
-		return earthYears / 29.447498
-	case "uranus":
-		return earthYears / 84.016846
-	case "neptune":
-		return earthYears / 164.79132
-	}
-	return earthYears
+	return earthYears / orbitalPeridods[planet]
 }
