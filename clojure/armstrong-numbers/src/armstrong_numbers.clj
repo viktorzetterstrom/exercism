855d07d9226c7d2de50c84973296c24c7e25fb39
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-01-10 12:44:47 +0100

    clojure armstrong-numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,13 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn exp [x n]
+  (reduce * (repeat n x)))
+
+(defn digits [num]
+  (map #(Character/digit % 10) (seq (str num))))
+
+(defn armstrong? [num]
+  (== num
+    (reduce + 
+      (map #(exp (bigint %) (bigint (count (str num))))
+        (digits num)))))
