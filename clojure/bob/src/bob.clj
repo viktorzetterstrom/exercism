4d0c4696a76593240f4da7cbad31796afd4ccf62
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-01-14 08:18:06 +0100

    clojure bob refactor

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,11 +1,16 @@
 (ns bob)
 
+(defn askYell? [s] (re-find #"^(?=.*[A-Z])[^a-z]+\?+$" s))
+(defn yell? [s] (re-find #"\?\s*$" s))
+(defn ask? [s] (re-find #"^(?=.*[A-Z])[^a-z]+$" s))
+(defn silence? [s] (re-find #"^\s*$" s))
+
 (defn response-for [s]
   (cond
-    (re-find #"^(?=.*[A-Z])[^a-z]+\?+$" s) "Calm down, I know what I'm doing!"
-    (re-find #"\?\s*$" s) "Sure."
-    (re-find #"^(?=.*[A-Z])[^a-z]+$" s) "Whoa, chill out!"
-    (re-find #"^\s*$" s) "Fine. Be that way!"
+    (askYell? s) "Calm down, I know what I'm doing!"
+    (yell? s) "Sure."
+    (ask? s) "Whoa, chill out!"
+    (silence? s) "Fine. Be that way!"
     :else "Whatever."
     )
   )
