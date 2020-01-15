9559ff98e587709818792e86911d10bafdf3fa00
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-01-15 13:10:54 +0100

    clojure isbn-verifier

---
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -0,0 +1,20 @@
+(ns isbn-verifier)
+
+(defn str-to-isbn-list
+  [s]
+  (map #(if (= % "X") 10 (Long/parseLong %)) (re-seq #"[\d]|X$" s)))
+
+(defn isbn-sum
+  [isbn-list]
+  (mod
+   (reduce +
+           (map #(* %1 %2)
+                isbn-list
+                (range 10 0 -1)))
+   11))
+
+(defn isbn?
+  [isbn]
+  (if (= (count (str-to-isbn-list isbn)) 10)
+    (= (isbn-sum (str-to-isbn-list isbn)) 0)
+    false))
