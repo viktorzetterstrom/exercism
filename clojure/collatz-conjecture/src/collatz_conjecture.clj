533c1f3375b1ed574f02a5bd419913d5086d9129
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-01-17 08:08:31 +0100

    clojure collatz-conjecture cleanup

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -12,14 +12,9 @@
   [num]
   (if (even? num) (even-collatz-fn num) (odd-collatz-fn num)))
 
-(defn calc-collatz-step
-  ([num]
-   (if (= num 1)
-     0
-     (calc-collatz-step (pick-collatz-fn num) 0)))
+(defn collatz
+  ([num] (collatz num 0))
   ([num, step]
    (if (= num 1)
-     (+ step 1)
-     (calc-collatz-step (pick-collatz-fn num) (+ step 1)))))
-
-(defn collatz [num] (calc-collatz-step num))
+     step
+     (collatz (pick-collatz-fn num) (inc step)))))
