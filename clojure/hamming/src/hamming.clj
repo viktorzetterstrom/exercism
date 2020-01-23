c76fad408da43bed0b473704a9a8b2783a117ebe
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-01-23 08:07:27 +0100

    clojure hamming fix

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -2,7 +2,7 @@
 
 (defn distance [strand1 strand2]
   (if (= (count strand1) (count strand2))
-    (->> (map #(list %1 %2) (seq strand1) (seq strand2))
-         (map #(if (apply = %) 0 1))
-         (reduce +))
+    (->> (map vector strand1 strand2)
+         (map (fn [[a b]] (if (= a b) 0 1)))
+         (apply +))
     nil))
