9d9f3a501c750d9b61be4561b84b96d4f0dca482
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-03-02 12:56:58 +0100

    js rotational-cipher improvements

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,21 +1,13 @@
-const ignoredChars = " 12345678'!.,";
-
-const charBaseValue = char => {
-  if ("abcdefghijklmnopqrstuvwxyz".includes(char)) return 97;
-  else if ("ABCDEFGIHJKLMNOPQRSTUVWXYZ".includes(char)) return 65;
-  else return 0;
-};
+const charBaseValue = char =>
+  /[A-Z]/.test(char) ? "A".charCodeAt(0) : "a".charCodeAt(0);
 
 const calcCharCode = (char, inc) =>
   charBaseValue(char) + ((char.charCodeAt(0) - charBaseValue(char) + inc) % 26);
 
-const incChar = (char, inc) =>
-  ignoredChars.includes(char)
-    ? char
-    : String.fromCharCode(calcCharCode(char, inc));
-
 export class RotationalCipher {
   static rotate(str, inc) {
-    return [...str].map(char => incChar(char, inc)).join("");
+    return str.replace(/[a-z]/gi, char =>
+      String.fromCharCode(calcCharCode(char, inc))
+    );
   }
 }
