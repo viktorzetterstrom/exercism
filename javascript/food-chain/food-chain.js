3eac3184117bdb3b8a690b902c9f04f28ef1367b
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-05-31 11:30:47 +0200

    js food-chain refactor

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -34,30 +34,45 @@ const FOOD = Object.freeze([
   },
 ]);
 
-export class Song {
-  verse(nr) {
-    const food = nr !== 8 ? FOOD.slice(0, nr).reverse() : FOOD.slice(7, 8);
+const swallowFirst = ({ type }) =>
+  `I know an old lady who swallowed a ${type}.`;
 
-    let verse = "";
-    for (let i = 0; i < food.length; i++) {
+const swallow = (current, previous, includeExtra) => {
+  let str = `She swallowed the ${previous.type} to catch the ${current.type}`;
+  if (includeExtra && current.extra) str += ` ${current.extra}`;
+  str += ".";
+  return str;
+};
+
+export class Song {
+  verse(verseNumber) {
+    if (verseNumber < 1 || 8 < verseNumber) {
+      throw new Error("Invalid verse number");
+    }
+    if (verseNumber === 8) {
+      const horse = FOOD[verseNumber - 1];
+      return `${swallowFirst(horse)}\n${horse.comment}\n`;
+    }
+
+    const food = FOOD.slice(0, verseNumber).reverse();
+    let lines = [];
+    const firstFood = food[0];
+    lines.push(swallowFirst(firstFood));
+    lines.push(firstFood.comment);
+
+    for (let i = 1; i < food.length; i++) {
       const currentFood = food[i];
       const previousFood = food[i - 1];
-
-      let foodLine = !previousFood
-        ? `I know an old lady who swallowed a ${currentFood.type}`
-        : `She swallowed the ${previousFood.type} to catch the ${currentFood.type}`;
-      foodLine += i > 0 && currentFood.extra ? ` ${currentFood.extra}` : "";
-      foodLine += ".";
-
-      const lines = [foodLine];
-      if (i === 0 || i === nr - 1) {
-        lines.push(currentFood.comment);
-      }
-      lines.push(""); // Has to end with empty line
-
-      verse += lines.join("\n");
+      const includeExtra = i > 0;
+      lines.push(swallow(currentFood, previousFood, includeExtra));
     }
-    return verse;
+
+    if (verseNumber !== 1) {
+      lines.push(food[food.length - 1].comment);
+    }
+
+    lines.push(""); // Has to end with empty line
+    return lines.join("\n");
   }
 
   verses(from, to) {
