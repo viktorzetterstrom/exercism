dc29a67c5ed07a9dc4ffc8f9316bff721848c688
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-06-05 15:43:17 +0200

    js proverb a little bit better

--- javascript/proverb/proverb.js
+++ javascript/proverb/proverb.js
@@ -1,15 +1,13 @@
 export const proverb = (...args) => {
   const qualifier = args[args.length - 1].qualifier
-    ? args.pop().qualifier
-    : null;
+    ? args.pop().qualifier + " "
+    : "";
 
   let str = "";
   for (let i = 0; i < args.length - 1; i++) {
     str += `For want of a ${args[i]} the ${args[i + 1]} was lost.\n`;
   }
 
-  str += `And all for the want of a ${qualifier ? qualifier + " " : ""}${
-    args[0]
-  }.`;
+  str += `And all for the want of a ${qualifier}${args[0]}.`;
   return str;
 };
