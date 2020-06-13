883159e1feedcc295eece3770aa082b3805efad3
Viktor Zetterström <viktorzetterstrom@gmail.com>
2020-06-13 10:35:28 +0200

    ts resistor-color-duo

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,27 @@
+const ResistorColorValues = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+export class ResistorColor {
+  private colors: string[];
+  constructor(colors: string[]) {
+    if (colors.length < 2) {
+      throw new Error("At least two colors need to be present");
+    }
+    this.colors = colors.slice(0, 2);
+  }
+
+  value = (): number =>
+    Number(
+      this.colors.map((color) => ResistorColorValues.indexOf(color)).join("")
+    );
+}
