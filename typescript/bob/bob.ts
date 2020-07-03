822728ffec36358005ef6a3abbbc9ca7d857763b
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-07-03 14:24:23 +0200

    ts bob refactor

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,17 +1,23 @@
 class Bob {
   hey(statement: string): string {
-    const isScreaming = /^(?=.*[A-Z])[^a-z]+!?$/.test(statement);
-    const isQuestion = /\?\s*$/.test(statement);
-    const isSilence = /^\s*$/.test(statement);
-    const isForceFulQuestion = isScreaming && isQuestion;
-
-    if (isForceFulQuestion) return "Calm down, I know what I'm doing!";
-    if (isScreaming) return "Whoa, chill out!";
-    if (isQuestion) return "Sure.";
-    if (isSilence) return "Fine. Be that way!";
+    if (Bob.isForceFulQuestion(statement))
+      return "Calm down, I know what I'm doing!";
+    if (Bob.isScreaming(statement)) return "Whoa, chill out!";
+    if (Bob.isQuestion(statement)) return "Sure.";
+    if (Bob.isSilence(statement)) return "Fine. Be that way!";
 
     return "Whatever.";
   }
+
+  static isScreaming = (statement: string): boolean =>
+    /^(?=.*[A-Z])[^a-z]+!?$/.test(statement);
+
+  static isQuestion = (statement: string): boolean => /\?\s*$/.test(statement);
+
+  static isSilence = (statement: string): boolean => /^\s*$/.test(statement);
+
+  static isForceFulQuestion = (statement: string): boolean =>
+    Bob.isScreaming(statement) && Bob.isQuestion(statement);
 }
 
 export default Bob;
