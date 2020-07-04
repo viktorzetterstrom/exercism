09309077f537a3b2ecb0c602a8150be8c4e16a19
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-07-04 12:37:41 +0200

    ts matrix

---
+++ typescript/matrix/matrix.ts
@@ -0,0 +1,17 @@
+type MatrixString = string;
+
+class Matrix {
+  public rows: number[][];
+
+  constructor(matrix: MatrixString) {
+    this.rows = matrix.split("\n").map((row) => row.split(" ").map(Number));
+  }
+
+  get columns(): number[][] {
+    return this.rows[0].map((_, colIndex) =>
+      this.rows.map((row) => row[colIndex])
+    );
+  }
+}
+
+export default Matrix;
