918440f0c0eb03a41a30bd01623755f24135c39d
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-07-05 15:50:46 +0200

    ts matrix refactor

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -4,13 +4,19 @@ class Matrix {
   readonly rows: number[][];
 
   constructor(matrix: MatrixString) {
-    this.rows = matrix.split("\n").map((row) => row.split(" ").map(Number));
+    this.rows = this.createRows(matrix);
   }
 
   get columns(): number[][] {
-    return this.rows[0].map((_, colIndex) =>
-      this.rows.map((row) => row[colIndex])
-    );
+    return this.transpose(this.rows);
+  }
+
+  private createRows(matrix: MatrixString): number[][] {
+    return matrix.split("\n").map((row) => row.split(" ").map(Number));
+  }
+
+  private transpose(matrix: number[][]): number[][] {
+    return matrix[0].map((_, index) => matrix.map((row) => row[index]));
   }
 }
 
