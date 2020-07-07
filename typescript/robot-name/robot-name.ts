10e73487e6ac1bac3d156d631e4b36494f5c6ab1
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-07-07 16:58:47 +0200

    ts robot-name refactor

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,43 +1,44 @@
+function generateRandomNumber(min: number, max: number): number {
+  const difference = max - min;
+  return min + Math.floor(Math.random() * difference + 1); // min <= num <= max
+}
+
 class RobotName {
-  private static usedNames = new Set();
+  private static usedNames = new Set<string>();
   private readonly A_CHARCODE = 62;
   private readonly Z_CHARCODE = 90;
-  public name: string;
+  private _name = "";
 
   constructor() {
-    this.name = this.generateName();
+    this.setNewName();
+  }
+
+  get name(): string {
+    return this._name;
   }
 
   public resetName(): void {
-    this.name = this.generateName();
+    this.setNewName();
+  }
+
+  private setNewName(): void {
+    let name = this.generateName();
+    while (RobotName.usedNames.has(name)) {
+      name = this.generateName();
+    }
+    RobotName.usedNames.add(name);
+    this._name = name;
   }
 
   private generateName(): string {
-    let uniqueName = false;
-    let name = "";
-    while (!uniqueName) {
-      const letters = this.generateRandomLetter() + this.generateRandomLetter();
-      const numbers = this.generateRandomNumber(0, 999);
-      name = letters + String(numbers).padStart(3, "0");
-      uniqueName = !RobotName.usedNames.has(name);
-    }
-
-    RobotName.usedNames.add(name);
-    return name;
+    const letters = this.generateRandomLetter() + this.generateRandomLetter();
+    const numbers = generateRandomNumber(0, 999);
+    return letters + String(numbers).padStart(3, "0");
   }
 
   private generateRandomLetter(): string {
-    const charCode = this.generateRandomNumber(
-      this.A_CHARCODE,
-      this.Z_CHARCODE
-    );
+    const charCode = generateRandomNumber(this.A_CHARCODE, this.Z_CHARCODE);
     return String.fromCharCode(charCode);
-  }
-
-  // Generates number between: min <= digit <= max
-  private generateRandomNumber(min: number, max: number): number {
-    const difference = max - min;
-    return min + Math.floor(Math.random() * difference + 1);
   }
 }
 
