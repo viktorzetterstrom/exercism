6287948dd5b41077956efec76c5aeec80a935fd8
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-04-21 09:33:28 +0200

    elixir rna-transcription, refactor according to mentor feedback

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -1,20 +1,5 @@
 defmodule RnaTranscription do
   @doc """
-  Transcribes a single DNA nucleotide to its RNA complement
-  """
-  @type dna_nucleotide :: ?G | ?C | ?T | ?A
-  @type rna_complement :: ?C | ?G | ?A | ?U
-  @spec translate_dna_nucleotide(dna_nucleotide()) :: rna_complement()
-  def translate_dna_nucleotide(s) do
-    case s do
-      ?G -> ?C
-      ?C -> ?G
-      ?T -> ?A
-      ?A -> ?U
-    end
-  end
-
-  @doc """
   Transcribes a character list representing DNA nucleotides to RNA
 
   ## Examples
@@ -24,6 +9,13 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    Enum.map(dna, &RnaTranscription.translate_dna_nucleotide/1)
+    Enum.map(dna, fn nucleotide ->
+      case nucleotide do
+        ?G -> ?C
+        ?C -> ?G
+        ?T -> ?A
+        ?A -> ?U
+      end
+    end)
   end
 end
