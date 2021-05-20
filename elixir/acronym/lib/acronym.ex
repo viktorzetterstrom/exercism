97422f943db378081987850dbb0c190b06c69736
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-05-20 08:07:20 +0200

    elixir acronym, improvement

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -6,14 +6,8 @@ defmodule Acronym do
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
     Regex.split(~r/[ _-]+|[a-z](?=[A-Z])/, string)
-    |> Enum.map(&first_letter_upcased/1)
+    |> Enum.map(&String.first/1)
     |> Enum.join()
+    |> String.upcase()
   end
-
-  @spec first_letter_upcased(String.t()) :: String.t()
-  defp first_letter_upcased(string),
-    do:
-      string
-      |> String.first()
-      |> String.upcase()
 end
