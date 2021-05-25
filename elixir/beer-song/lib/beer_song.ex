e700c343c25ef0d12765628d006e2b2dae3f426b
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-05-25 16:11:07 +0200

    elixir beer-song, improvements

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,37 +3,34 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) when number > 2,
-    do: """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-    """
-
-  def verse(number) when number === 2,
-    do: """
-    2 bottles of beer on the wall, 2 bottles of beer.
-    Take one down and pass it around, 1 bottle of beer on the wall.
-    """
-
-  def verse(number) when number === 1,
-    do: """
-    1 bottle of beer on the wall, 1 bottle of beer.
-    Take it down and pass it around, no more bottles of beer on the wall.
-    """
-
-  def verse(number) when number === 0,
+  def verse(0),
     do: """
     No more bottles of beer on the wall, no more bottles of beer.
     Go to the store and buy some more, 99 bottles of beer on the wall.
     """
 
+  def verse(1),
+    do: """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+
+  def verse(2),
+    do: """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+
+  def verse(number),
+    do: """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
+
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
-  @spec lyrics(Range.t()) :: String.t()
-  def lyrics(), do: lyrics(99..0)
-
-  def lyrics(range) do
-    range |> Stream.map(&verse/1) |> Enum.join("\n")
+  def lyrics(range \\ 99..0) do
+    Enum.map_join(range, "\n", &verse/1)
   end
 end
