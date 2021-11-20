57b3a40af84c89bb74b148d30c7770b0654d822f
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-11-20 14:16:39 +0100

    kotlin: resistor-color

---
+++ kotlin/resistor-color/src/main/kotlin/ResistorColor.kt
@@ -0,0 +1,7 @@
+object ResistorColor {
+    private val colors = listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
+
+    fun colorCode(input: String): Int = colors.indexOf(input)
+
+    fun colors(): List<String> = colors
+}
